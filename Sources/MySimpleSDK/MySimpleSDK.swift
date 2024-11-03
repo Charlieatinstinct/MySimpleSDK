b62111ceae9910df5d7c9444d74e79f76feca364
Charlieatinstinct <102666182+Charlieatinstinct@users.noreply.github.com>
2024-11-03 10:13:48 +0800

    ds

--- Sources/MySimpleSDK/MySimpleSDK.swift
+++ Sources/MySimpleSDK/MySimpleSDK.swift
@@ -1,10 +1,27 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public struct Greeting {
-    public init() {}
+public class SecretHolder{
     
-    public func hello() -> String {
-        return "Hello from MySimpleSDK"
+    private var secrets = ["She has broke the most beautiful vast to his grandma", "There is no secret to tell."]
+    private var usedIndex = 0
+    
+    public func fetchSecret()->String {
+        if (usedIndex == secrets.count){
+            return ""
+        }else{
+            let secrect = secrets[usedIndex]
+            usedIndex += 1
+            return secrect
+        }
+    }
+    
+    public func recieveSecrect(secret: String) -> Bool{
+        if secrets.contains(secret){
+            return false
+        }else{
+            secrets.append(secret)
+            return true
+        }
     }
 }
