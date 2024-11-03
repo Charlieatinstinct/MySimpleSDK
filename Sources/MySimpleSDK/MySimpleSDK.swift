a8d304dc2e1c1749688a82d6b7a1af6d2407295c
Charlieatinstinct <102666182+Charlieatinstinct@users.noreply.github.com>
2024-11-03 10:06:36 +0800

    Initial commit

---
+++ Sources/MySimpleSDK/MySimpleSDK.swift
@@ -0,0 +1,10 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct Greeting {
+    public init() {}
+    
+    public func hello() -> String {
+        return "Hello from MySimpleSDK"
+    }
+}
